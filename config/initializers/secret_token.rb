b9078fe6ee9da195ecac6d16daf7352391116278
Andreea <gavrilovici_deea@yahoo.com>
2013-01-12 09:43:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BandApp::Application.config.secret_token = 'd60d62e30b339e7014ef43d0e737c12063e2812d9c27b32a6065f5500c5688c6d290f2e6adc7ca8240c887e7521708bd849ce76609c97fcb9341b47c8dd16382'
